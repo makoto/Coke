48a68fb3ca81e5d27cbd130cc30a1548b3f36de8
Makoto Inoue <inouemak@googlemail.com>
2014-12-07 14:32:47 +0000

    Change Tweet.fetch to create tweet object

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,17 @@
 class Tweet < ActiveRecord::Base
 
 	def self.fetch
-    JSON.parse RestClient.get(CONFIG["api_url"] + "/tweets.json")
+    json = JSON.parse RestClient.get(CONFIG["api_url"] + "/tweets.json")
+    json.map do |t|
+      self.new({
+        created_at:      t["created_at"],
+        updated_at:      t["updated_at"],
+        message:         t["message"],
+        followers:       t["followers"],
+        message_id:      t["message_id"],
+        sentiment_score: t["sentiment"],
+        user_handle:     t["user_handle"]
+      })
+    end
   end
-
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Tweet, :type => :model do
   context 'fetch' do
-    it "fetches tweets from API" do
+    let(:fetched) {
       VCR.use_cassette('tweets') do
-        expect(Tweet.fetch.count).to eql 2
+        Tweet.fetch
       end
+    }
+
+    it "fetches tweets from API" do
+      expect(fetched.count).to eql 2
+    end
+
+    it "converts into tweet object" do
+      expect(fetched.first).to be_a Tweet
     end
   end
 end
