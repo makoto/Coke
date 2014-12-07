d277fc98b3a7734132409696b11c191fbc1a253f
Makoto Inoue <inouemak@googlemail.com>
2014-12-07 14:50:54 +0000

    Handle Twitter API error message

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,9 @@
 class TweetsController < ApplicationController
+  def index
+    begin
+      @tweets = Tweet.fetch
+    rescue RestClient::InternalServerError => e
+      flash[:alert] = "There were problem getting response from twitter. Try again."
+    end
+  end
 end
--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe TweetsController, :type => :controller do
+  it "returns success" do
+    VCR.use_cassette('tweets') do
+      get 'index'
+      expect(response).to be_success
+    end
+  end
 
+  it "returns failed message" do
+    VCR.use_cassette('api_error') do
+      get 'index'
+      expect(response).to be_success
+      expect(flash[:alert]).to eq "There were problem getting response from twitter. Try again."
+    end
+  end
 end
