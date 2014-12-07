dcd8376241184a4e24cbc150280b72cae3e760a2
Makoto Inoue <inouemak@googlemail.com>
2014-12-07 13:40:04 +0000

    Add tweet migration

--- db/migrate/20141207132923_create_tweets.rb
+++ db/migrate/20141207132923_create_tweets.rb
@@ -1,6 +1,13 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
+      t.integer :followers
+      t.integer :message_id
+      t.string :message
+      t.float  :sentiment_score
+      t.string :user_handle
+      # contains original tweet json from API
+      t.text   :body
       t.timestamps
     end
   end
