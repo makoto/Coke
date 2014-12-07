b2c9c14b185d256e7e1f218c734d1cfff7af33c8
Makoto Inoue <inouemak@googlemail.com>
2014-12-07 14:04:24 +0000

    Implement Tweet.fetch to load tweets from API

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,7 @@
 class Tweet < ActiveRecord::Base
+
+	def self.fetch
+    JSON.parse RestClient.get(CONFIG["api_url"] + "/tweets.json")
+  end
+
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'fetch' do
+    it "fetches tweets from API" do
+      VCR.use_cassette('tweets') do
+        expect(Tweet.fetch.count).to eql 2
+      end
+    end
+  end
 end
